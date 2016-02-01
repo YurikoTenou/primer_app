c21725da65b94c79856cab05e9dcb26aa15790f0
Paola Muñoz <Yuriko@ihonahans-imac.local>
2016-01-31 19:00:56 -0500

    Pruebas en la app

--- Hola Mundo/ViewController.swift
+++ Hola Mundo/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBOutlet weak var messageLabel: UILabel!
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeMessage(sender: AnyObject) {
+        messageLabel.text = "Hola \(nameTextField.text!)"
+        view.endEditing(true)
+    }
 
 }
+//Comentario salvaje aparece
+
 
